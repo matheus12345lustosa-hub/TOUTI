339486564eec7c359202c6a98eaab99feb0d7170
Matheus Cardim <matheus12345lustosa@gmail.com>
2026-01-19 10:19:50 -0300

    Fix deprecated eslint config in next.config.js

--- next.config.js
+++ next.config.js
@@ -2,11 +2,11 @@
 const nextConfig = {
     // Enable standalone build for Vercel/Docker optimizations
     output: 'standalone',
-    eslint: {
-        // Warning: This allows production builds to successfully complete even if
-        // your project has ESLint errors.
-        ignoreDuringBuilds: true,
-    },
+    // eslint: {
+    //     // Warning: This allows production builds to successfully complete even if
+    //     // your project has ESLint errors.
+    //     ignoreDuringBuilds: true,
+    // },
     typescript: {
         // !! WARN !!
         // Dangerously allow production builds to successfully complete even if
